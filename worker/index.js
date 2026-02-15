8014017cfa25e7efce7e41f64a15c92f48dc4721
Christian Rose <120071468+CRose958@users.noreply.github.com>
2026-02-15 13:17:19 -0500

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -12,7 +12,7 @@ export default {
 		const corsHeaders = {
 			'Access-Control-Allow-Origin': '*',
 			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type',
+			'Access-Control-Allow-Headers': 'Content-Type, X-Queue-Pin, X-Queue-Admin',
 		};
 
 		// Handle CORS preflight
